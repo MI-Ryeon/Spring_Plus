efe1ea15d88055a8f765be7d7d93b473b17fb870
Ahn Jeongmin <133615790+MI-Ryeon@users.noreply.github.com>
2023-07-27 18:18:58 +0900

    init commit

---
+++ src/main/java/com/sparta/spring_plus/SpringPlusApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_plus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/spring_plus/SpringPlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_plus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPlusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
